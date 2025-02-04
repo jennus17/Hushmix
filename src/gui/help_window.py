8a80af7b578156fbb95777e464fc66b00ccf99e0
jennus17 <joao.bento17.pt@gmail.com>
2025-02-04 22:42:16 +0000

    Enhance HelpWindow with improved styling and command descriptions

--- src/gui/help_window.py
+++ src/gui/help_window.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 from utils.icon_manager import IconManager
+import gui.app as app
 
 class HelpWindow:
     def __init__(self, parent):
@@ -10,6 +11,9 @@ class HelpWindow:
 
         self.window.transient(parent)
         self.window.grab_set()
+
+        self.accent_color = app.get_windows_accent_color()
+        self.normal_font_size = 14
 
         self.setup_gui()
         self.center_window(parent)
@@ -34,28 +38,49 @@ class HelpWindow:
         )
         self.frame.pack(expand=True, fill="both")
 
-        self.help_label = ctk.CTkLabel(
+        # Bold label for "Special commands:"
+        self.special_commands_label = ctk.CTkLabel(
+            self.frame, 
+            text="Special commands:", 
+            font=("Segoe UI", self.normal_font_size + 6, "bold"),
+            text_color=self.accent_color
+        )
+        self.special_commands_label.pack(anchor="w", padx=10, pady=5)
+
+        # Create labels for commands with descriptions
+        self.commands = [
+            ("master", "Controls the speaker volume"),
+            ("system", "Controls the system sounds volume"),
+            ("mic", "Controls the default microphone"),
+            ("current", "Controls the current application in focus")
+        ]
+        
+        for command, description in self.commands:
+            command_frame = ctk.CTkFrame(self.frame)
+            command_frame.pack(anchor="w", padx=10)
+
+            self.command_label = ctk.CTkLabel(
+                command_frame, 
+                text=f"• {command}:", 
+                text_color=self.accent_color,
+                font=("Segoe UI", self.normal_font_size + 2, "bold")
+                )
+            self.command_label.pack(side="left") 
+
+            self.description_label = ctk.CTkLabel(
+                command_frame, 
+                text=description,
+                font=("Segoe UI", self.normal_font_size)
+                )
+            self.description_label.pack(side="left", padx=(10, 0))
+
+        self.extra_text_label = ctk.CTkLabel(
             self.frame,
-            text=self.get_help_text(),
-            font=("Segoe UI", 14),
-            justify=ctk.LEFT,
+            text="For specific applications, use the full name\n(e.g., chrome.exe, discord.exe, etc.)\n\nYou can also group applications by using a comma\n(e.g.,App 1: chrome.exe, discord.exe, master, etc.)",
+            font=("Segoe UI", self.normal_font_size),
+            justify="center"
         )
-        self.help_label.pack(pady=10, padx=10)
-
-
-    @staticmethod
-    def get_help_text():
-        """Return help text for the application."""
-        return (
-            "Special commands:"
-            "\n• master - Controls the speaker volume"
-            "\n• system - Controls the system sounds volume"
-            "\n• mic - Controls the default microphone"
-            "\n• current - Controls the current application" 
-            "\n\t in focus"
-            "\n\n For specific applications, use the full name\n"         
-            "       (e.g., chrome.exe, discord.exe, etc.)"
-        )
+        self.extra_text_label.pack(anchor="center", padx=5, pady=10)
 
     def center_window(self, parent):
         self.window.update_idletasks()
